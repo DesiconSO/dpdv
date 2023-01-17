1f9751399d63f6dedfd4576644d2a3f542e2c6a7
VictordaSilvaf <victordasilvafernandes@gmail.com>
2023-01-17 12:54:45 -0300

    Bug: Adicionando cascadeOnDelete

--- database/migrations/2022_11_07_000003_create_products_table.php
+++ database/migrations/2022_11_07_000003_create_products_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
 
     public function down(): void
     {
+        Schema::dropIfExists('propostal_products');
         Schema::dropIfExists('products');
     }
 };
