95907ccc86a62378fc9c82c2564240d559f69e9f
VictordaSilvaf <victordasilvafernandes@gmail.com>
2023-01-17 12:56:12 -0300

    Bug: Adicionando cascadeOnDelete

--- database/migrations/2022_11_07_000006_proposal_products.php
+++ database/migrations/2022_11_07_000006_proposal_products.php
@@ -12,9 +12,15 @@ return new class extends Migration
     {
         Schema::create('proposal_products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('proposal_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('proposal_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('product_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
             $table->float('discount', 10, 2)->default(0);
             $table->string('amount');
             $table->float('total', 10, 2);
@@ -24,6 +30,9 @@ return new class extends Migration
 
     public function down(): void
     {
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('proposals');
         Schema::dropIfExists('propostal_products');
     }
 };
